fe388b97e4410f3cf85d195431321cf6d5be4649
Andrzej Pigoń <andrzej.pigon.external@zf.com>
2026-02-04 19:20:31 +0100

    add user tutorial

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,11 +1,14 @@
 import type { MouseEvent } from "react";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { useNotes } from "../../hooks/useNotes";
 import { Note } from "../Note/Note";
 import { TrashZone } from "../TrashZone/TrashZone";
+import { Tutorial } from "../Tutorial";
 import styles from "./Canvas.module.css";
 
 export function Canvas() {
   const { notes, addNote, draggingNoteId } = useNotes();
+  const [tutorialSeen, setTutorialSeen] = useLocalStorage("tutorialSeen", false);
 
   const handleClick = (e: MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget && draggingNoteId === null) {
@@ -19,6 +22,7 @@ export function Canvas() {
         <Note key={note.id} note={note} />
       ))}
       <TrashZone />
+      {!tutorialSeen && <Tutorial onDismiss={() => setTutorialSeen(true)} />}
     </div>
   );
 }
